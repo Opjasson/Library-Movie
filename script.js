30ddfbdd89fb9e14a2c8a5d3c6c2d0c2d7289628
Opjasson <azmitheusos333@gmail.com>
2024-09-06 11:49:26 +0700

    second commit

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
 const http = require("http");
 const port = 3000;
-const fs = require('fs')
+const fs = require("fs");
 
-const renderHtml = (path,res) => {
-    fs.readFile(path, (err, data)=>{
-        if(err){
-            res.writeHead(404)
-            res.write('Error: file not found')
-        }else {
-            res.write(data)
+
+// function Render html
+const renderHtml = (path, res) => {
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            res.writeHead(404);
+            res.write("Error: file not found");
+        } else {
+            res.write(data);
         }
-        res.end()
-    })
-}
-
+        res.end();
+    });
+};
 
 http.createServer((req, res) => {
     res.writeHead(200, {
-        'Content-Type' : 'text/html'
-    })
-    
-    const url = req.url
-    if(url == '/contact'){
-        renderHtml('./contact.html',res)
-    }else if(url == '/about'){
-        renderHtml('./about.html',res)
-    }else {
-        renderHtml('./index.html',res)
-    }
+        "Content-Type": "text/html",
+    });
 
+    const url = req.url;
+    if (url == "/contact") {
+        renderHtml("./contact.html", res);
+    } else if (url == "/about") {
+        renderHtml("./about.html", res);
+    } else {
+        renderHtml("./index.html", res);
+    }
 }).listen(port, () => {
     console.log(`Server running in port ${port}...`);
 });
+
+
